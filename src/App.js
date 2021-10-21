2a1f56dd84224a145d7f7497e822e2ca84ea30ca
Sergio Tapia <tapia24@hotmail.com>
2021-10-20 19:45:56 -0500

    Landing page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+//import Button from '@material-ui/core/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container fluid="md" className="App">
+        <h1> CreativesFans </h1>
+        <h3>Acepta donaciones de tus fans y <br></br> 
+        sigue financiando tu trabajo creativo.</h3>
+      
+        <Row>
+          
+          <Col>  
+            <button className="App-login">
+            Iniciar Sesión
+            </button>
+          </Col>
+
+          <Col>
+            <button className="App-create-profile">
+            Crear perfil →
+            </button>
+          </Col>
+
+        </Row>
+        <Row>
+          <Col>
+            <button className="App-login">
+            🔍 Creadores
+            </button>
+          </Col>
+        </Row>
+      </Container>
+
   );
 }
 
