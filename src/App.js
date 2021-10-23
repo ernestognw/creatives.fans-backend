1d239d7f085468d991e1e390556807c9df201ec1
ernestognw <ernestognw@gmail.com>
2021-10-23 16:53:07 -0500

    Reset to base

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import './App.css';
-import {Container} from 'react-bootstrap';
-import {Row} from 'react-bootstrap';
-import {Col} from 'react-bootstrap';
-//import Button from '@material-ui/core/Button'
-
-function App() {
+const App = () => {
   return (
-      <Container fluid="md" className="App">
-        <h1> CreativesFans </h1>
-        <h3>Acepta donaciones de tus fans y <br></br> 
-        sigue financiando tu trabajo creativo.</h3>
-      
-        <Row>
-          
-          <Col>  
-            <button className="App-login">
-            Iniciar Sesión
-            </button>
-          </Col>
-
-          <Col>
-            <button className="App-create-profile">
-            Crear perfil →
-            </button>
-          </Col>
-
-        </Row>
-        <Row>
-          <Col>
-            <button className="App-login">
-            🔍 Creadores
-            </button>
-          </Col>
-        </Row>
-      </Container>
-
+    <>
+      <p>Hello wolrd</p>
+    </>
   );
-}
+};
 
 export default App;
