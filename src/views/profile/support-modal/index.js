bd2c2b78e2c983fd4f2b6e400a63cfbe044e1164
ernestognw <ernestognw@gmail.com>
2021-11-30 21:55:55 -0600

    Feature: Added payment on support

--- src/views/profile/support-modal/index.js
+++ src/views/profile/support-modal/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { useUser } from "@providers/user";
 import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
 import {
   Modal,
   ModalOverlay,
@@ -13,13 +15,34 @@ import {
   FormControl,
   FormLabel,
   Textarea,
+  Checkbox,
+  Flex,
+  Text,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  useToast,
 } from "@chakra-ui/react";
 import { SUPPORT } from "./requests";
+import { routes } from "@config/constants";
+import PaymentCard from "@components/payment-card";
 import { GET_SUPPORTS_RECEIVED } from "../received/requests";
 
 const SupportModal = ({ isOpen, onClose, creative, refetch }) => {
   const [submitting, setSubmitting] = useState(false);
+  const [tip, setTip] = useState(10);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(0);
+  const [sendTip, setSendTip] = useState(true);
   const [description, setDescription] = useState("");
+  const toast = useToast();
+  const {
+    user: { stripeCustomer },
+  } = useUser();
+
+  const noPaymentMethods =
+    !stripeCustomer || stripeCustomer?.paymentMethods.length === 0;
 
   const [support] = useMutation(SUPPORT, {
     refetchQueries: [GET_SUPPORTS_RECEIVED],
@@ -27,17 +50,46 @@ const SupportModal = ({ isOpen, onClose, creative, refetch }) => {
 
   const sendSupport = async () => {
     setSubmitting(true);
-    await support({
-      variables: {
-        support: {
-          creative,
-          description,
-        },
+
+    const variables = {
+      support: {
+        creative,
+        description,
       },
-    });
-    await refetch();
-    onClose();
-    setDescription("");
+    };
+
+    if (sendTip) {
+      variables.support.amount = Number(tip);
+      variables.support.paymentMethod =
+        stripeCustomer.paymentMethods[selectedPaymentMethod].id;
+    }
+
+    const { errors } = await support({ variables });
+
+    if (errors) {
+      toast({
+        title: "Ocurrió un error",
+        description: errors[0].message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      if (sendTip) {
+        toast({
+          title: `¡Gracias por apoyar!`,
+          description: "Tu aportación fue recibida",
+          status: "error",
+          isClosable: true,
+        });
+      }
+      await refetch();
+      onClose();
+      setDescription("");
+      setTip(0);
+      setSelectedPaymentMethod(0);
+      setSendTip(true);
+    }
+
     setSubmitting(false);
   };
 
@@ -56,6 +108,62 @@ const SupportModal = ({ isOpen, onClose, creative, refetch }) => {
               placeholder="Escribe un mensaje..."
             />
           </FormControl>
+          <Flex flexDirection="column">
+            <Checkbox
+              my={3}
+              onChange={({ target: { checked } }) => setSendTip(checked)}
+              isDisabled={noPaymentMethods}
+              isChecked={sendTip}
+            >
+              {noPaymentMethods
+                ? "No has agregado métodos de pago"
+                : "Agregar propina"}
+            </Checkbox>
+            {noPaymentMethods && (
+              <Link to={routes.DASHBOARD.PAYMENT_METHODS.ADD}>
+                <Button colorScheme="yellow" variant="link">
+                  Agregar método de pago
+                </Button>
+              </Link>
+            )}
+            {sendTip && !noPaymentMethods && (
+              <>
+                <FormControl mb={4} id="amount">
+                  <FormLabel>Cantidad (MXN)</FormLabel>
+                  <NumberInput
+                    onChange={(val) => setTip(val.replace(/^\$/, ""))}
+                    value={`$${tip}`}
+                    min={10}
+                  >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+                <Text mb={3} color="gray.500">
+                  Selecciona tu método de pago
+                </Text>
+                {stripeCustomer?.paymentMethods.map(({ id, card }, index) => (
+                  <PaymentCard
+                    key={id}
+                    brand={card.brand}
+                    expirationYear={card.expirationYear}
+                    expirationMonth={card.expirationMonth}
+                    funding={card.funding}
+                    last4={card.last4}
+                    mb={3}
+                    cursor="pointer"
+                    borderColor={
+                      index === selectedPaymentMethod ? "green" : undefined
+                    }
+                    onClick={() => setSelectedPaymentMethod(index)}
+                  />
+                ))}
+              </>
+            )}
+          </Flex>
         </ModalBody>
         <ModalFooter>
           <Button mr={3} onClick={onClose} variant="ghost">
